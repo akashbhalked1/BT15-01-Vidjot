1ae64df0dde109e32db5507cc707c1f8f1b4104e
Chi Lin <chilin89117@gmail.com>
2018-01-15 16:03:30 -0800

    Fixed db connect string

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 let db = {};
 if(process.env.NODE_ENV === 'production') {
   // see https://mlab.com
-  db = {mongoURI: "mongodb://abbie:123456@ds257627.mlab.com:57627/vidjot-prod}"};
+  db = {mongoURI: "mongodb://abbie:123456@ds257627.mlab.com:57627/vidjot-prod"};
 } else {
   db = {mongoURI: "mongodb://localhost:27017/vidjot-dev"};
 }
