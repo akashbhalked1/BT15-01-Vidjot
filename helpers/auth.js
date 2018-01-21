5e062861b3fe32048267ea0e4f69f709508782e7
Chi Lin <chilin89117@gmail.com>
2018-01-20 21:53:44 -0800

    Added 'assets' branch and .png files

--- helpers/auth.js
+++
@@ -1,7 +0,0 @@
-let ensureAuth = (req, res, next) => {
-  if(req.isAuthenticated()) return next();
-  req.flash('error', 'Not authorized!');
-  res.redirect('/users/login');
-};
-
-module.exports = {ensureAuth};
